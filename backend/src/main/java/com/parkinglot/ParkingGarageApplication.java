5d56561b323b1db7e5bd6f622eded5388882562c
Purnima Srikanth <purnimasrikanth@Purnimas-MacBook-Pro.local>
2025-12-10 17:50:55 -0600

    Initial commit before Java upgrade

---
+++ backend/src/main/java/com/parkinglot/ParkingGarageApplication.java
@@ -0,0 +1,11 @@
+package com.parkinglot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingGarageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingGarageApplication.class, args);
+    }
+}
---
+++ backend/src/main/java/com/parkinglot/model/User.java
@@ -0,0 +1,36 @@
+package com.parkinglot.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String firstName;
+    
+    @Column(nullable = false)
+    private String lastName;
+    
+    @Column(nullable = false, unique = true)
+    private String email;
+    
+    @Column(nullable = false)
+    private String password;
+    
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Car> cars = new ArrayList<>();
+}
